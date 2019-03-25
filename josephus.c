4e2312161e266f5fd0c87af773c6db8deaa39133
zh <zh@xrc.com>
2019-03-25 14:08:33 +0800

    Program 3.9 - Circular list Josephus problem

---
+++ josephus.c
@@ -0,0 +1,36 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int Item;
+typedef struct node *link;
+struct node {
+  Item item;
+  link next;
+};
+
+int main(int argc, char *argv[]) {
+  if (argc < 3) {
+    printf("not enough arguments\n");
+    exit(-1);
+  }
+  int N = atoi(argv[1]), M = atoi(argv[2]);
+  printf("N=%d M=%d\n", N, M);
+  link t = malloc(sizeof *t), x = t;
+  t->item = 1;
+  t->next = t;
+  for (int i = 2; i <= N; i++) {
+    x = (x->next = malloc(sizeof *x));
+    x->item = i;
+    x->next = t;
+  }
+  while (x->next != x) {
+    for (int i = 1; i < M; i++) {
+      x = x->next;
+    }
+    printf("remove: %d\n", x->next->item);
+    x->next = x->next->next;
+  }
+  printf("last: %d\n", x->item);
+  return 0;
+}
