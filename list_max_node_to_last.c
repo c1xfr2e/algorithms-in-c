7a5b5481a272a2345afaf296f33a81d6527100d9
bluecover <gurity@163.com>
2019-03-27 00:32:08 +0800

    Exercise 3.34 - Move largest item on a list to last

---
+++ list_max_node_to_last.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "list_node.h"
+
+int main(int argc, char *argv[]) {
+  int N = atoi(argv[1]);
+  link head = malloc(sizeof *head), last = head;
+  for (int i = 0; i < N; i++) {
+    last = (last->next = malloc(sizeof *last));
+    last->item = atoi(argv[i + 2]);
+    last->next = NULL;
+  }
+
+  int max = head->next->item;
+  link x = head;
+  for (link n = head->next; n->next != NULL; n = n->next) {
+    if (n->next->item > max) {
+      max = n->next->item;
+      x = n;
+    }
+  }
+
+  last->next = x->next;
+  last = x->next;
+  x->next = x->next->next;
+  last->next = NULL;
+
+  for (link n = head->next; n != NULL; n = n->next) {
+    printf("%d ", n->item);
+  }
+
+  return 0;
+}
