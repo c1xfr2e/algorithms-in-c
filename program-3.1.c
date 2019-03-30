4b19f875ff42427996d3423690ec0ff66c4102ee
bluecover <gurity@163.com>
2019-03-30 22:01:28 +0800

    program 3.1 - function definition

---
+++ program-3.1.c
@@ -0,0 +1,22 @@
+// Program 3.1
+// Function definition
+
+#include <stdio.h>
+
+int lg(int);
+
+int main() {
+  int i, N;
+  for (i = 0, N = 10; i <= 6; i++, N *= 10) {
+    printf("%7d %2d %9d\n", N, lg(N), N * lg(N));
+  }
+  return 0;
+}
+
+int lg(int N) {
+  int i;
+  for (i = 0; N > 0; i++, N /= 2) {
+  }
+  return i;
+}
+
