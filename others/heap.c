0a32a2126b79d3eac1fc60ef7ddb0f9a248305ff
zh <zh@xrc.com>
2019-04-01 21:27:23 +0800

    heap sort

--- others/heap.c
+++ others/heap.c
@@ -38,14 +38,31 @@ void sift_up(Item *heap, int start, int N) {
     sift_down(heap, x, start);
 }
 
+void swap(Item *heap, int i, int j) {
+    Item t = heap[i];
+    heap[i] = heap[j];
+    heap[j] = heap[i];
+}
+
+void heap_sort(Item *heap, int N) {
+    int M = N;
+    while (M > 0) {
+        printf("%d ", heap[0]);
+        swap(heap, 0, M - 1);
+        sift_up(heap, 0, M--);
+    }
+}
+
 int main(int argc, char *argv[]) {
-    int N = argc - 1;
+    int N = argc - 1, i;
     Item *heap = malloc(N * sizeof(Item));
-    for (int i = 0; i < N; i++)
+    for (i = 0; i < N; i++)
         heap[i] = atoi(argv[i + 1]);
-    for (int i = N / 2 - 1; i >= 0; i--)
+    for (i = N / 2 - 1; i >= 0; i--)
         sift_up(heap, i, N);
-    for (int i = 0; i < N; i++)
+    for (i = 0; i < N; i++)
         printf("%d ", heap[i]);
+    printf("\n");
+    heap_sort(heap, N);
     return 0;
 }
