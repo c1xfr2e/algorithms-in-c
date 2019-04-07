da75989e3ca990771ec866fd438632b74ac1723f
zh <zh@xrc.com>
2019-04-07 22:03:02 +0800

    KMP: generate next index array

---
+++ others/kmp.c
@@ -0,0 +1,35 @@
+
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void kmp_gen_next(char *s, int *N, int len) {
+    N[0] = 0;
+    int i, k;
+    for (i = 1; i < len; i++) {
+        k = N[i - 1];
+        while (true) {
+            if (s[i] == s[k]) {
+                break;
+            }
+            if (--k == -1)
+                break;
+            k = N[k];
+        }
+        N[i] = k + 1;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    char *s = argv[1];
+    int i, len = strlen(s);
+    int *N = malloc(len * sizeof(int));
+    kmp_gen_next(s, N, len);
+    for (char *c = s; *c; c++) {
+        printf("%2c ", *c);
+    }
+    printf("\n");
+    for (i = 0; i < len; i++)
+        printf("%2d ", N[i]);
+}
