fcc22807cbed2006b216d02ac8509c0ce24dd0a4
zh <zh@xrc.com>
2019-04-20 16:27:15 +0800

    minor fix

--- others/insert-ordered-array.c
+++ others/insert-ordered-array.c
@@ -2,8 +2,9 @@
 
 typedef int (*PREV)(int);
 
-void insert_ordered_array(int arr[], int first, int pos, int item, PREV prev) {
+void insert_ordered_array(int arr[], int first, int pos, PREV prev) {
     int x = pos, y = prev(x);
+    int item = arr[x];
     while (y >= first) {
         if (item < arr[y]) {
             arr[x] = arr[y];
@@ -26,7 +27,7 @@ int heap_prev(int i) {
 void test_array_insert() {
     int a[] = {1, 2, 4, 5, 3};
     int x = 4;
-    insert_ordered_array(a, 0, x, a[x], array_prev);
+    insert_ordered_array(a, 0, x, array_prev);
     for (int i = 0; i < 5; i++)
         printf("%d ", a[i]);
 }
@@ -35,7 +36,7 @@ void test_heap_insert() {
     int a[22];
     a[0] = 1, a[1] = 3, a[4] = 4, a[10] = 5, a[21] = 2;
     int x = 21;
-    insert_ordered_array(a, 0, x, a[x], heap_prev);
+    insert_ordered_array(a, 0, x, heap_prev);
     printf("%d %d %d %d %d\n", a[0], a[1], a[4], a[10], a[21]);
 }
 
