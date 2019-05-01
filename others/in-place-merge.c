fe213f49d0de2b9f76ab2f534998a36f98aa3235
bluecover <gurity@163.com>
2019-05-01 12:09:09 +0800

    in-place-merge: attempt 2

--- others/in-place-merge.c
+++ others/in-place-merge.c
@@ -29,8 +29,7 @@ void reverse(int A[], int L, int R) {
 }
 
 void rotate(int A[], int L, int M, int R) {
-    printf("rotate %d %d %d\n", L, M, R);
-    if (M - L <= 0 || R - M <= 0)
+    if (M - L <= 0 || R - M < 0)
         return;
     reverse(A, L, M - 1);
     reverse(A, M, R);
@@ -53,7 +52,7 @@ int lower_bound(int A[], int v, int L, int R) {
 
 // merge sorted sub-array A[L,M-1] and A[M,R]
 void in_place_merge(int A[], int L, int M, int R) {
-    if (M <= L || M >= R)
+    if (M <= L || M > R)
         return;
     if (R - L == 1) {
         if (A[R] < A[L])
@@ -64,25 +63,19 @@ void in_place_merge(int A[], int L, int M, int R) {
     rotate(A, p, M, q - 1);
     int new_m = p + (q - M);
     in_place_merge(A, L, p, new_m - 1);
-    in_place_merge(A, new_m, q, R);
+    if (new_m == p)
+        in_place_merge(A, p + 1, q, R);
+    else
+        in_place_merge(A, new_m, q, R);
 }
 
-int input1[] = {0, 1, 5, 2, 3, 4};
+int input1[] = {3, 4, 5, 7, 9, 0, 1, 2, 6, 8};
+int input2[] = {3, 4, 5, 6, 13, 14, 15, 0, 1, 2, 7, 8, 9, 10};
 
 int main(int argc, char *argv[]) {
-    in_place_merge(input1, 0, 3, 5);
-    print(input1, 6);
-    return 0;
-    int input2[] = {3, 4, 5, 9, 10, 0, 1, 2, 6, 8};
-    in_place_merge(input2, 0, 5, 9);
-    print(input2, 10);
-    int input3[] = {3, 4, 5, 6, 13, 14, 15, 0, 1, 2, 7, 8, 9, 10};
-    int N = 14, m = 7;
-    in_place_merge(input3, 0, m, N - 1);
-    print(input3, 14);
-    // rotate(a, 0, 4, 6);
-    // for (int j = 0; j < N; j++) {
-    //     printf("%d ", a[j]);
-    // }
+    in_place_merge(input1, 0, 5, 9);
+    print(input1, 10);
+    in_place_merge(input2, 0, 7, 13);
+    print(input2, 14);
     return 0;
 }
